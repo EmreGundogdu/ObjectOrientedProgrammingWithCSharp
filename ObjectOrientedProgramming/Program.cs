7da18a6074982ef68e2bfe917ff92d7bc9138ca2
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-05 11:23:20 +0300

    OOP Property

--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -1,3 +1,33 @@
-﻿#region Sınıf Nedir? 
-
+﻿#region Field
+//Field : Nesne içerisinde değer tutmamızı sağlayan alanlardır.
+//Class içerisinde tanımlanan fieldlara kendi türün default değerler verilir. Metot içerisinde tanımlanan değişkenlere default değer verilmez.
 #endregion
+#region Property
+//Nesne içerisinde özellik/property sağlar.
+//Property esasında özünde bir metottur. Yani programatik/algoritmik kodlarımızı inşa ettiğimiz bir metot.
+#endregion
+#region Encapsulation(Kapsülleme/Sarmalama)
+//Encapsulation, bir nesne içerisindeki dataların(field'lardaki verilen) dışarıya kontrollü bir şekilde açılmasını ve kontrollü bir şekilde veri almasıdır.
+#endregion
+MyClass myClass = new MyClass(); //Nesne oluşturma
+class MyClass
+{ 
+    int a; //Field
+    string b; //Field
+    #region Full Property
+    //Property hangi türden bir field'i temsil ediyorsa o türden olmalıdır.
+    //Propertyler temsil ettikleri field'ların isimlerinin başharfi büyük olacak şekilde isimlendirilir
+    public int Adi
+    {
+        get
+        {
+            //Property üzerinden değer talep edildiğinde bu blok tetiklenir. Yani değer buradan gönderilir.
+
+        }
+        set
+        {
+
+        }
+    }
+    #endregion
+}
