b7aea6c776e7829cb2eac3ed365ef3ff8670600e
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-05 11:49:05 +0300

    Prop property

--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -10,24 +10,51 @@
 //Encapsulation, bir nesne içerisindeki dataların(field'lardaki verilen) dışarıya kontrollü bir şekilde açılmasını ve kontrollü bir şekilde veri almasıdır.
 #endregion
 MyClass myClass = new MyClass(); //Nesne oluşturma
+Console.WriteLine(myClass.Yasi); //get blogu tetiklenir
+myClass.Yasi = 21; //set blogu tetiklenir.
+
 class MyClass
-{ 
-    int a; //Field
+{
+    int yasi; //Field
     string b; //Field
     #region Full Property
     //Property hangi türden bir field'i temsil ediyorsa o türden olmalıdır.
     //Propertyler temsil ettikleri field'ların isimlerinin başharfi büyük olacak şekilde isimlendirilir
-    public int Adi
+    public int Yasi
     {
         get
         {
             //Property üzerinden değer talep edildiğinde bu blok tetiklenir. Yani değer buradan gönderilir.
-
+            return yasi;
         }
         set
         {
-
+            yasi = value;
         }
     }
     #endregion
+    #region Prop Property
+    //Field'i otomatik olarak tanımlar"adi" fieldini tanımlar. Küçük harfle ve get ve set işlemlerini kendi içinde tanımlar.
+    public int Adi { get; set; } 
+    #endregion
+}
+class Banka
+{
+    int bakiye;
+    public int Bakiye
+    {
+        get
+        {
+            if (bakiye > 0)
+                return bakiye * 10 / 100;
+            return 5;
+        }
+        set
+        {
+            if (value < 10)
+                bakiye = value;
+            else
+                bakiye = value * 95 / 100;
+        }
+    }
 }
