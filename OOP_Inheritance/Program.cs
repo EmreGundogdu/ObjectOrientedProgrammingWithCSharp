a8f2d38725685f8dcd81ce2560a6e4de94068666
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-09 17:24:57 +0300

    Kalıtım Ornek

--- OOP_Inheritance/Program.cs
+++ OOP_Inheritance/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Muhasabeci muhasabeci = new();
+Mudur muhasabeci = new();
+class Personel
+{
+    public string Adi { get; set; }
+    public string Soyadi { get; set; }
+    public bool MedeniHal { get; set; }
+}
+class Muhasabeci : Personel
+{
+
+}
+class Yazıilimci : Personel
+{
+
+}
+class Mudur : Personel
+{
+
+}
