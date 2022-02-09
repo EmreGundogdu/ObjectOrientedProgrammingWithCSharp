276f42f783bed9c81202e22ead082256d2cdd4f1
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-09 19:11:31 +0300

    Inheritance örnek

--- OOP_Inheritance/Program.cs
+++ OOP_Inheritance/Program.cs
@@ -68,11 +68,30 @@ class MyClass
     {
 
     }
+    public MyClass()
+    {
+
+    }
+    public MyClass(string a)
+    {
+
+    }
+    public MyClass(int a, string b)
+    {
+
+    }
 }
 class MyClass2 : MyClass
 {
-    //Bir class üzerinden nesne oluşturulurken o nesnenin üst classında ki bir constructoru seçip ona göre bir üst class'tan nesne üretmek istersek eğer, alt classa gelip constructor kısmına 'base' keywordünü kullanıp üst class'ta bulunan constructor'a parametreleri veriyoruz...Base keywordünü kullanmadığımı zaman alt class'tan nesne oluşturacağımız zaman üst classtan da nesne oluşturacağı için ve üst classta parametre alan bir constructor olduğunu varsayarsak bizden nesne oluştururken bizden parametre vermemizi isteyecektir ve bize hata vericektir.
-    public MyClass2() : base(5)
+    //Bir class üzerinden nesne oluşturulurken o nesnenin base classında ki bir constructoru seçip ona göre bir base class'tan nesne üretmek istersek eğer, derived classa gelip constructor kısmına 'base' keywordünü kullanıp base class'ta bulunan constructor'a parametreleri veriyoruz...Base keywordünü kullanmadığımı zaman derived class'tan nesne oluşturacağımız zaman base classtan da nesne oluşturacağı için ve base classta parametre alan bir constructor olduğunu varsayarsak bizden nesne oluştururken bizden parametre vermemizi isteyecektir ve bize hata vericektir.
+    //Eğer ki base class'ta boş parametreli bir constructor varsa derived classta base ile bir bildirimde bulunmak zorunda değiliz.... Çünkü varsayılan olarak kalıtımsal durumda base classtaki boş parametreleri constructor tetiklenir.
+    //Bir class'ın constructorunın yanında : base( ... ) keywordünü kullanırsak eğer o classın base classının tüm constructorlarını bize getirecektir. Haliyle ilgili sınıftan bir nesne üretilirken base classtan nesne üretimi esnasında hangi constructorun tetikleyeceğini bu şekilde belirleyebiliriz yani overloadlarına bakarız.
+    public MyClass2():base(5,"sdasdsadasdsa")//nesne oluştururken bu constructor tetiklenicek
+    {
+
+    }
+    //Eğer ki base class'ın constructoru sadece parametre alan bir constructor ise deriverd class(alt class)'larda o constructor'a bir değer göndermek zorundayız. Bunuda base keywordüyle sağlıyoruz.
+    public MyClass2(int a) : base(a)
     {
 
     }
