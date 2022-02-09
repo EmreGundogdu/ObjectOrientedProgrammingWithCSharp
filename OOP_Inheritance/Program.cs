4e0a42deda29100da8da55512526b67207bb4d97
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-09 19:04:30 +0300

    Inheritance base keywordü

--- OOP_Inheritance/Program.cs
+++ OOP_Inheritance/Program.cs
@@ -2,6 +2,7 @@
 Mudur Mudur = new();
 //Bir class'ın sadece bir tane Base Class'ı olur
 //Bir sınıftan nesne üretimi yapılırken kalıtım aldığı üst sınıflar varsa eğer önce o sınıflardan SIRAYLA nesne üretilir. Örnek : C classı B classının alt classı olsun ve B class'ı da A class'ının alt classı olsun. C classından nesne üretmek istediğimiz zaman Önce A, sonra B ve sonra da C classlarından sıraysıyla nesneler üretilecektir.
+new D();
 class Personel
 {
     public string Adi { get; set; }
@@ -59,5 +60,20 @@ class D : C
     public D()
     {
         Console.WriteLine($"{nameof(D)} nesnesi oluşturuldu");
+    }
+}
+class MyClass
+{
+    public MyClass(int a)
+    {
+
+    }
+}
+class MyClass2 : MyClass
+{
+    //Bir class üzerinden nesne oluşturulurken o nesnenin üst classında ki bir constructoru seçip ona göre bir üst class'tan nesne üretmek istersek eğer, alt classa gelip constructor kısmına 'base' keywordünü kullanıp üst class'ta bulunan constructor'a parametreleri veriyoruz...Base keywordünü kullanmadığımı zaman alt class'tan nesne oluşturacağımız zaman üst classtan da nesne oluşturacağı için ve üst classta parametre alan bir constructor olduğunu varsayarsak bizden nesne oluştururken bizden parametre vermemizi isteyecektir ve bize hata vericektir.
+    public MyClass2() : base(5)
+    {
+
     }
 }
