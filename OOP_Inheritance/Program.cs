e95f6895ce57f6d47cedf962bcc72f26a912e9a8
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-09 18:44:03 +0300

    Kalıtımda nesne üretimi

--- OOP_Inheritance/Program.cs
+++ OOP_Inheritance/Program.cs
@@ -1,5 +1,7 @@
 ﻿Muhasabeci muhasabeci = new();
 Mudur Mudur = new();
+//Bir class'ın sadece bir tane Base Class'ı olur
+//Bir sınıftan nesne üretimi yapılırken kalıtım aldığı üst sınıflar varsa eğer önce o sınıflardan SIRAYLA nesne üretilir. Örnek : C classı B classının alt classı olsun ve B class'ı da A class'ının alt classı olsun. C classından nesne üretmek istediğimiz zaman Önce A, sonra B ve sonra da C classlarından sıraysıyla nesneler üretilecektir.
 class Personel
 {
     public string Adi { get; set; }
