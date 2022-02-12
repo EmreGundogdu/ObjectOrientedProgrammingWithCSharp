668334a6f79331b41f4d6cedf716d0c0d3c41f19
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-12 20:55:38 +0300

    Sanal yapilar ornek

--- OOP_SanalYapilar/Program.cs
+++ OOP_SanalYapilar/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Kalem kalem = new();
+kalem.Bilgi();
+class Obje
+{
+    public virtual void Bilgi()
+    {
+        Console.WriteLine("Ben bir objeyim");
+    }
+}
+class Kalem : Obje
+{
+    public override void Bilgi()
+    {
+        Console.WriteLine("Ben bir Kalemim");
+    }
+}
