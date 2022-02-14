ac5f172682bb6dc0dc09a1db915362008f92a2dc
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-14 20:32:09 +0300

    Polimorfizm Örnek

--- OOP_Polimorfizm/Program.cs
+++ OOP_Polimorfizm/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿//Bir nesnenin, birden fazla referansla işaretlenmesi; o nesnenin, birden fazla türün davranışlarını gösterebilmesini sağlar
+MyClass myClass = new MyClass();
+class MyClass : MyClass2
+{
+
+}
+class MyClass2
+{
+
+}
